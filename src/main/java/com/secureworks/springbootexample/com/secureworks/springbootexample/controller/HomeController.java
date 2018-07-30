97a1d11d2135c6d68a05a669f475f47ecc9908dd
ajaygupta0512 <ajayindfw@gmail.com>
2018-07-29 23:59:38 -0400

    Initial commit.

---
+++ src/main/java/com/secureworks/springbootexample/com/secureworks/springbootexample/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.secureworks.springbootexample.com.secureworks.springbootexample.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        return "Dou Boot, reporting for duty";
+    }
+}
